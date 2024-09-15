b254f977210c1ac1b89bf69162bc21051dd7862c
Satyam Singh <satyamsinghrajput9980@gmail.com>
2024-09-15 12:24:58 +0530

    Create OOPS.java

---
+++ OOPS.java
@@ -0,0 +1,17 @@
+1) Class -  Class is userdefined blueprint from which objects are created .
+          EX : 
+               Public class Main{
+  //  data members
+  
+}
+
+2)  Objects - Objects are instances of classes that are created to use attribute and methods of class.
+            EX - 
+            Class Objects {
+             name : Satyam;
+             class: BE;
+}
+
+3) Data Encapsulation - Data Encapsulations is defined as a wrapping of the data and methods under a single unit .
+                        Data Encapsulations is implemented using getters and Setters methods.
+
